93946f06a9623e546573b067a497526811823929
Curtis <curtis@tinbrain.net>
2014-05-28 13:21:06 +1000

    Make Admin more usable

--- lot/admin.py
+++ lot/admin.py
@@ -3,6 +3,8 @@ from .models import LOT
 
 
 class LOTAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'user', 'type', 'created',)
+    list_filter = ('type',)
     readonly_fields = ('uuid',)
 
 admin.site.register(LOT, LOTAdmin)
