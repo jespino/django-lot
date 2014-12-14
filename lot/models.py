81bf256f9d4e9d564ea79624f948860ab68073bd
n3storm <nestor@barriolinux.es>
2014-12-14 12:29:09 +0100

    compatible user model loading 1.6, 1.7

--- lot/models.py
+++ lot/models.py
@@ -4,7 +4,16 @@ from django.db import models
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+except:
+    from django.apps import apps
+    user_app, user_model = settings.AUTH_USER_MODEL.split('.')
+    User = apps.get_app_config(user_app).get_model(user_model)
+
 from django.utils.timezone import now
+
 
 LOT_SETTINGS = getattr(settings, 'LOT', {
     'fast-login': {
@@ -33,7 +42,7 @@ class LOT(models.Model):
     uuid = models.CharField(_('UUID'), max_length=50)
     type = models.SlugField(_('LOT type'), max_length=50,
                             choices=LOT_TYPE_CHOICES)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('user'))
+    user = models.ForeignKey(User, verbose_name=_('user'))
     session_data = models.TextField(_('Jsoned Session Data'), blank=True)
     created = models.DateTimeField(_('Creation date'), auto_now_add=True)
     next_url = models.URLField(blank=True)
