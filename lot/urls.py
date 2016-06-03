c6298a573dc3188b8c57954287d78e7da253483a
Jarek Glowacki <jarekwg@gmail.com>
2016-06-04 00:06:55 +1000

    Update to new-style urlpatterns format

--- lot/urls.py
+++ lot/urls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from . import views
 
-urlpatterns = patterns("",
+urlpatterns = [
     url(r"^login/(?P<uuid>[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12})/$", views.LOTLogin.as_view(), name="login"),
-)
+]
