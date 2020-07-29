55fd085fa10775f4d6f4455eef29d6c6f6ad1015
Marc Fister <mwfister@gmail.com>
2020-07-29 14:51:30 +1000

    is_safe_url is deprecated, drop support for django < 3.0

--- lot/views.py
+++ lot/views.py
@@ -1,12 +1,11 @@
 import json
 
 from django.conf import settings
-from django.http import HttpResponseRedirect, HttpResponseNotFound
-from django.shortcuts import get_object_or_404, resolve_url
-from django.utils.http import is_safe_url
-from django.views.generic import View
-
 from django.contrib.auth import authenticate, login
+from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, resolve_url
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.views.generic import View
 
 from .models import LOT
 
@@ -38,7 +37,7 @@ class LOTLogin(View):
         if lot.next_url:
             redirect_to = resolve_url(lot.next_url)
 
-        if not is_safe_url(url=redirect_to, allowed_hosts=[request.get_host()]):
+        if not url_has_allowed_host_and_scheme(url=redirect_to, allowed_hosts=[request.get_host()]):
             redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
 
         return HttpResponseRedirect(redirect_to)
